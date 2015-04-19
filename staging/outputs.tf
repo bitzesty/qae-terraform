a4e5887bdf6abe21d80adae57a448b2c67eb1d1f
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-19 18:36:05 +0300

    [STAGING] corrected outputs

--- staging/outputs.tf
+++ staging/outputs.tf
@@ -2,10 +2,14 @@ output "address" {
   value = "${aws_elb.staging_load_balancer.dns_name}"
 }
 
+output "vs_address" {
+  value = "${aws_elb.virus_scanner_staging_load_balancer.dns_name}"
+}
+
 output "lb_id" {
   value = "${aws_elb.staging_load_balancer.id}"
 }
 
-output "vs_lb" {
+output "vs_id" {
   value = "${aws_elb.virus_scanner_staging_load_balancer.id}"
 }
