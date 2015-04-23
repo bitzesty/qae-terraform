09bfca6781cbbd0d5fdb626fc79106fd894e36e6
Ruslan Khamidullin <rusllonrails@gmail.com>
2015-04-23 18:44:30 +0300

    Clean up

--- production old/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value = "${aws_elb.production_load_balancer.dns_name}"
-}
-output "lb_id" {
-  value = "${aws_elb.production_load_balancer.id}"
-}
-# output "virus_scanner_elastic_ip" {
-#   value = "${aws_eip.virus_scanner_elastic_ip.public_ip}"
-# }
